3802b6f255987a5f279268ba4b6ec80a761576ba
LinsThi <thiagolins13255@gmail.com>
2022-03-31 13:33:46 -0300

    feat: :sparkles: Created Routes app

--- src/shared/constantes/routes/index.ts
+++ src/shared/constantes/routes/index.ts
@@ -1,4 +1,4 @@
-export const HOME_SCREEN = 'HomeScreen';
-export const SEARCH_SCREEN = 'SearchScreen';
-export const LIBRARY_SCREEN = 'Librarycreen';
-export const PREMIUM_SCREEN = 'PremiumScreen';
+export const HOME_SCREEN = 'Inicio';
+export const SEARCH_SCREEN = 'Busca';
+export const LIBRARY_SCREEN = 'Sua Biblioteca';
+export const PREMIUM_SCREEN = 'Premium';
---
+++ src/shared/theme/index.ts
@@ -0,0 +1,11 @@
+export default {
+  Colors: {
+    BLACK: '#121212',
+    DARK_PINK: '#C31f8A',
+    WHITE: '#FFFFFF',
+  },
+
+  FontSizes: {
+    LABEL_TAB_SIZE: 15,
+  },
+};
