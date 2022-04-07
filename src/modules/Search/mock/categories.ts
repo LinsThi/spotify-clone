034ab5f9a11cada4dbb62de24e11e8dbe23979c2
LinsThi <thiagolins13255@gmail.com>
2022-04-06 21:23:19 -0300

    :feat: :sparkles: SearchScreen component

---
+++ src/modules/Search/mock/categories.ts
@@ -0,0 +1,162 @@
+export default [
+  {
+    id: '1',
+    title: 'Happy Vibes',
+    albuns: [
+      {
+        id: 1,
+        nameAlbum: 'Album',
+        imageUri:
+          'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+        backgroundColor: '#8c67ac',
+      },
+      {
+        id: 2,
+        nameAlbum: 'Album',
+        imageUri:
+          'https://cdn6.f-cdn.com/contestentries/1485199/27006121/5ca3e39ced7f1_thumb900.jpg',
+        backgroundColor: '#b95d06',
+      },
+    ],
+  },
+  {
+    id: '2',
+    title: 'Popular Playlists',
+    albuns: [
+      {
+        id: 1,
+        nameAlbum: 'Album',
+        imageUri:
+          'https://cdn6.f-cdn.com/contestentries/1485199/27006121/5ca3e39ced7f1_thumb900.jpg',
+        backgroundColor: '#b95d06',
+      },
+      {
+        id: 2,
+        nameAlbum: 'Album',
+        imageUri:
+          'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+        backgroundColor: '#8c67ac',
+      },
+    ],
+  },
+  {
+    id: '3',
+    title: 'Shows to try',
+    albuns: [
+      {
+        id: 1,
+        nameAlbum: 'Album',
+        imageUri:
+          'https://images-na.ssl-images-amazon.com/images/I/61F66QURFyL.jpg',
+        backgroundColor: '#e13300',
+      },
+      {
+        id: 2,
+        nameAlbum: 'Album',
+        imageUri:
+          'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+        backgroundColor: '#8c67ac',
+      },
+    ],
+  },
+  {
+    id: '4',
+    title: 'Playlist One',
+    albuns: [
+      {
+        id: 1,
+        nameAlbum: 'Album',
+        imageUri:
+          'https://images-na.ssl-images-amazon.com/images/I/61F66QURFyL.jpg',
+        backgroundColor: '#1e3264',
+      },
+      {
+        id: 2,
+        nameAlbum: 'Album',
+        imageUri:
+          'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+        backgroundColor: '#b31149',
+      },
+    ],
+  },
+  {
+    id: '5',
+    title: 'Playlist Two',
+    albuns: [
+      {
+        id: 1,
+        nameAlbum: 'Album',
+        imageUri:
+          'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+        backgroundColor: '#b31149',
+      },
+      {
+        id: 2,
+        nameAlbum: 'Album',
+        imageUri:
+          'https://images-na.ssl-images-amazon.com/images/I/61F66QURFyL.jpg',
+        backgroundColor: '#1e3264',
+      },
+    ],
+  },
+  {
+    id: '6',
+    title: 'Playlist Three',
+    albuns: [
+      {
+        id: 1,
+        nameAlbum: 'Album',
+        imageUri:
+          'https://cdn6.f-cdn.com/contestentries/1485199/27006121/5ca3e39ced7f1_thumb900.jpg',
+        backgroundColor: '#8c67ac',
+      },
+      {
+        id: 2,
+        nameAlbum: 'Album',
+        imageUri:
+          'https://images-na.ssl-images-amazon.com/images/I/61F66QURFyL.jpg',
+        backgroundColor: '#1e3264',
+      },
+    ],
+  },
+  {
+    id: '7',
+    title: 'Playlist Four',
+    albuns: [
+      {
+        id: 1,
+        nameAlbum: 'Album',
+        imageUri:
+          'https://images-na.ssl-images-amazon.com/images/I/61F66QURFyL.jpg',
+        backgroundColor: '#1e3264',
+      },
+      {
+        id: 2,
+        nameAlbum: 'Album',
+        imageUri:
+          'https://cdn6.f-cdn.com/contestentries/1485199/27006121/5ca3e39ced7f1_thumb900.jpg',
+        backgroundColor: '#8c67ac',
+      },
+    ],
+  },
+  {
+    id: '8',
+    title: 'Playlist Five',
+    albuns: [
+      {
+        id: 1,
+        nameAlbum: 'Album',
+        imageUri:
+          'https://images-na.ssl-images-amazon.com/images/I/61F66QURFyL.jpg',
+        backgroundColor: '#8c67ac',
+      },
+      {
+        id: 2,
+        nameAlbum: 'Album',
+        imageUri:
+          'https://cdn6.f-cdn.com/contestentries/1485199/27006121/5ca3e39ced7f1_thumb900.jpg',
+        backgroundColor: '#8c67ac',
+      },
+    ],
+  },
+];
