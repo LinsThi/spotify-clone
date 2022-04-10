0c7e17f2daeab7fef2edeb543f3d1c0f47b2f617
LinsThi <thiagolins13255@gmail.com>
2022-04-10 20:38:01 -0300

    :lipstick: Fixing SearchScreen style

--- src/modules/Search/components/SearchBar/index.tsx
+++ src/modules/Search/components/SearchBar/index.tsx
@@ -1,20 +1,23 @@
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 import React, { useContext } from 'react';
 import { ThemeContext } from 'styled-components/native';
+
+import { SEARCH_BUTTON_SCREEN } from '~/shared/constantes/routes';
 
 import * as Sty from './styles';
 
 export function SearchBar() {
   const { Colors } = useContext(ThemeContext);
+  const navigation = useNavigation();
 
   return (
-    <Sty.Container>
+    <Sty.Container
+      onPress={() => navigation.navigate(SEARCH_BUTTON_SCREEN as never)}
+    >
       <Feather name="search" size={35} color={Colors.BLACK} />
 
-      <Sty.SearchInput
-        placeholder="Artistas, músicas ou podcasts"
-        placeholderTextColor={Colors.GRAY}
-      />
+      <Sty.SearchInput>Artistas, músicas ou podcasts</Sty.SearchInput>
     </Sty.Container>
   );
 }
