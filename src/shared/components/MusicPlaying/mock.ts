4fa079196273aa84dda0e0d275361a791875b9d3
LinsThi <thiagolins13255@gmail.com>
2022-04-18 21:36:50 -0300

    :sparkles: Created mock file

---
+++ src/shared/components/MusicPlaying/mock.ts
@@ -0,0 +1,6 @@
+export const MusicPlayingMock = {
+  name: "break up with your girlfriend, i'm bored",
+  artist: 'Ariana Grande',
+  imageUri: 'https://i.scdn.co/image/ab67616d0000b2735ef878a782c987d38d82b605',
+  backgroundColor: '#70313c',
+};
